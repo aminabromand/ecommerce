339704821453e3a0b7e71b43a30e91fb2d55a9e9
amin <amin.r.abromand@gmail.com>
2018-06-17 12:06:55 +0200

    completed part54

--- src/search/views.py
+++ src/search/views.py
@@ -6,11 +6,18 @@ from products.models import Product
 # Create your views here.
 class SearchProductView(ListView):
     #queryset = Product.objects.all()
-    template_name = 'products/list.html'
+    template_name = 'search/view.html'
+
+    def get_context_data(self, *args, **kwargs):
+    	context = super(SearchProductView, self).get_context_data(*args, **kwargs)
+    	query = self.request.GET.get('q')
+    	context['query'] = query
+    	# SerachQuery.objects.create(query=query) # I dont have this, so it is commented out
+    	return context
 	
     def get_queryset(self, *args, **kwargs):
         print("request: {0}".format(self.request.GET))
         query = self.request.GET.get('q')
         if query is not None:
         	return Product.objects.filter(title__icontains=query)
-        return Product.objects.none()
+        return Product.objects.featured()
