57b370b82be04ed94e64b1a96d3c70660f23ef40
amin <amin.r.abromand@gmail.com>
2018-06-23 20:50:16 +0200

    completed part67

--- src/apps/carts/models.py
+++ src/apps/carts/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models
-
+from django.db.models.signals import pre_save, post_save, m2m_changed
 
 from products.models import Product
 
@@ -37,6 +37,7 @@ class CartManager(models.Manager):
 class Cart(models.Model):
 	user = models.ForeignKey(User, null=True, blank=True)
 	products = models.ManyToManyField(Product, blank=True)
+	subtotal = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
 	total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
@@ -45,3 +46,26 @@ class Cart(models.Model):
 
 	def __str__(self):
 		return str(self.id)
+
+
+def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
+	# print('action')
+	if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+		# print(instance.products.all())
+		# print(instance.total)
+		products = instance.products.all()
+		total = 0
+		for x in products:
+			total += x.price
+		# print(total)
+		if instance.subtotal != total:
+			instance.subtotal = total
+			instance.save()
+
+m2m_changed.connect(m2m_changed_cart_receiver, sender=Cart.products.through)
+
+
+def pre_save_cart_receiver(sender, instance, *args, **kwargs):
+	instance.total = instance.subtotal + 10 # * 1.19
+
+pre_save.connect(pre_save_cart_receiver, sender=Cart)
--- src/apps/carts/views.py
+++ src/apps/carts/views.py
@@ -5,7 +5,8 @@ from .models import Cart
 # Create your views here.
 
 def cart_home(request):
-	cart_obj = Cart.objects.new_or_get(request)
+
+	# Old Code
 	# cart_id = request.session.get('cart_id', None)
 	# qs = Cart.objects.filter(id=cart_id)
 	# if qs.exists() and qs.count() == 1:
@@ -17,4 +18,13 @@ def cart_home(request):
 	# else:
 	# 	cart_obj = Cart.objects.new(user=request.user)
 	# 	request.session['cart_id'] = cart_obj.id
+
+	cart_obj, new_obj = Cart.objects.new_or_get(request)
+	products = cart_obj.products.all()
+	total = 0
+	for x in products:
+		total += x.price
+	print(total)
+	cart_obj.total = total
+	cart_obj.save()
 	return render(request, 'carts/home.html', {})
