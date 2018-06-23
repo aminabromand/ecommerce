12a609af6d417c70b3aeacef05c9bc8f02910072
amin <amin.r.abromand@gmail.com>
2018-06-24 00:05:43 +0200

    completed part70

--- src/apps/carts/views.py
+++ src/apps/carts/views.py
@@ -6,29 +6,9 @@ from .models import Cart
 # Create your views here.
 
 def cart_home(request):
-
-	# Old Code
-	# cart_id = request.session.get('cart_id', None)
-	# qs = Cart.objects.filter(id=cart_id)
-	# if qs.exists() and qs.count() == 1:
-	# 	cart_obj = qs.first()
-	# 	print('Cart ID exists {0}'.format(cart_id))
-	# 	if request.user.is_authenticated() and cart_obj.user is None:
-	# 		cart_obj.user = request.user
-	# 		cart_obj.save()
-	# else:
-	# 	cart_obj = Cart.objects.new(user=request.user)
-	# 	request.session['cart_id'] = cart_obj.id
-
 	cart_obj, new_obj = Cart.objects.new_or_get(request)
-	products = cart_obj.products.all()
-	total = 0
-	for x in products:
-		total += x.price
-	print(total)
-	cart_obj.total = total
-	cart_obj.save()
-	return render(request, 'carts/home.html', {})
+
+	return render(request, 'carts/home.html', {'cart': cart_obj})
 
 
 def cart_update(request):
