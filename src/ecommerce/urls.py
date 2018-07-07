c9538236c50af129fce0305ddd23476ed1bc4da1
amin <amin.r.abromand@gmail.com>
2018-07-07 19:02:52 +0200

    completed part 88 incorporated the address form into the checkout view added the address view/endpoint to create address in the database

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -33,6 +33,7 @@ from django.views.generic import TemplateView
 #     )
 
 from apps.accounts.views import login_page, register_page, guest_register_view
+from apps.addresses.views import checkout_address_create_view
 from .views import home_page, about_page, contact_page
 
 urlpatterns = [
@@ -40,6 +41,7 @@ urlpatterns = [
     url(r'^about/$', about_page, name='about'),
     url(r'^contact/$', contact_page, name='contact'),
     url(r'^login/$', login_page, name='login'),
+    url(r'^checkout/address/create/$', checkout_address_create_view, name='checkout_address_create'),
     url(r'^register/guest/$', guest_register_view, name='guest_register'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^register/$', register_page, name='register'),
