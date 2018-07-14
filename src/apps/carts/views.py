7a5faa7cd4a93952ebefc7a4298c3899faac6c9f
amin <amin.r.abromand@gmail.com>
2018-07-14 13:39:10 +0200

    completed part105 added ajax json response to cart update view adding removing product will not reload the page but change the button only

--- src/apps/carts/views.py
+++ src/apps/carts/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
 from apps.addresses.forms import AddressForm
@@ -18,8 +19,6 @@ def cart_home(request):
 
 
 def cart_update(request):
-	if request.is_ajax():
-		print('Ajax request')
 	print(request.POST)
 	product_id = request.POST.get('product_id')
 	if product_id is not None:
@@ -31,10 +30,19 @@ def cart_update(request):
 		cart_obj, new_obj = Cart.objects.new_or_get(request)
 		if product_obj in cart_obj.products.all():
 			cart_obj.products.remove(product_obj)
+			added = False
 		else:
 			cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
+			added = True
 		request.session['cart_items'] = cart_obj.products.count()
 	# return redirect(product_obj.get_absolute_url())
+		if request.is_ajax(): # Asynchronous JavaScript And XML / JSON
+			print('Ajax request')
+			json_data = {
+				"added": added,
+				"removed": not added,
+			}
+			return JsonResponse(json_data)
 	return redirect('cart:home')
 
 
