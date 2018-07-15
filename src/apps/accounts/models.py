784c2c367ecc1fff5de12d8653f71fbed698b248
amin <amin.r.abromand@gmail.com>
2018-07-15 23:35:15 +0200

    completed part 124 played around with required fields

--- src/apps/accounts/models.py
+++ src/apps/accounts/models.py
@@ -7,13 +7,16 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
 
-	def create_user(self, email, password=None, is_active=True, is_staff=False, is_admin=False):
+	def create_user(self, email, full_name=None, password=None, is_active=True, is_staff=False, is_admin=False):
 		if not email:
 			raise ValueError("Users must have an email address")
 		if not password:
 			raise ValueError("Users must have a password address")
+		# if not full_name:
+		# 	raise ValueError("Users must have a fullname")
 		user_obj = self.model(
-			email = self.normalize_email(email)
+			email = self.normalize_email(email),
+			full_name = full_name,
 		)
 		user_obj.set_password(password) # change user password as well
 		user_obj.staff = is_staff
@@ -22,17 +25,19 @@ class UserManager(BaseUserManager):
 		user_obj.save(using=self._db)
 		return user_obj
 
-	def create_staffuser(self, email, password=None):
+	def create_staffuser(self, email, full_name=None, password=None):
 		user = self.create_user(
 			email,
+			full_name=full_name,
 			password=password,
 			is_staff=True,
 		)
 		return user
 
-	def create_superuser(self, email, password=None):
+	def create_superuser(self, email, full_name=None, password=None):
 		user = self.create_user(
 			email,
+			full_name=full_name,
 			password=password,
 			is_staff=True,
 			is_admin=True,
@@ -43,7 +48,7 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser):
 	# username 	= models.CharField()
 	email 		= models.EmailField(max_length=255, unique=True)
-	# full_name 	= models.CharField(max_length=255, blank=True, null=True)
+	full_name 	= models.CharField(max_length=255, blank=True, null=True)
 	active 		= models.BooleanField(default=True)
 	staff		= models.BooleanField(default=False)
 	admin		= models.BooleanField(default=False)
@@ -60,6 +65,8 @@ class User(AbstractBaseUser):
 		return self.email
 
 	def get_full_name(self):
+		if self.full_name:
+			return self.full_name
 		return self.email
 
 	def get_short_name(self):
