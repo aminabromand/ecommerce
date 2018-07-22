1e7af99d73ee66284bd423c38e8936cf2f69f320
amin <amin.r.abromand@gmail.com>
2018-07-22 11:53:33 +0200

    completed part 137 added stripe customer id to billing profile model and added pre save signal to query stripe for the id

--- src/apps/billing/models.py
+++ src/apps/billing/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 
 import stripe
 
@@ -41,18 +41,24 @@ class BillingProfile(models.Model):
 	active = models.BooleanField(default=True)
 	update = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
-	# customer_id in Stripe or Braintree
+	customer_id = models.CharField(max_length=120, null=True, blank=True) # in Stripe or Braintree
 
 	def __str__(self):
 		return self.email
 
 	objects = BillingProfileManager()
 
-# def billing_profile_created_receiver(sender, instance, created, *args, **kwargs):
-# 	if created:
-# 		print('ACTUAL API REQUEST Send to stripe/braintree')
-# 		instance.customer_id = newID
-# 		instance.save()
+def billing_profile_creating_receiver(sender, instance, *args, **kwargs):
+	if not instance.customer_id and instance.email:
+		print('ACTUAL API REQUEST Send to stripe/braintree')
+		customer = stripe.Customer.create(
+				email = instance.email
+			)
+		print(customer)
+		instance.customer_id = customer.id
+
+
+pre_save.connect(billing_profile_creating_receiver, sender=BillingProfile)
 
 
 def user_created_receiver(sender, instance, created, *args, **kwargs):
