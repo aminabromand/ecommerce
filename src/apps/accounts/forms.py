c4ad8ca19e04aca7a1cb2d76580380a4cf72b077
amin <amin.r.abromand@gmail.com>
2018-08-25 19:11:44 +0200

    completed part 186 incorporated email activation into login process

--- src/apps/accounts/forms.py
+++ src/apps/accounts/forms.py
@@ -91,6 +91,31 @@ class LoginForm(forms.Form):
         data = self.cleaned_data
         email = data.get('email')
         password = data.get('password')
+
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            # user email is registered, check active/email activation
+            not_active = qs.filter(is_active=False)
+            if not_active.exists():
+                ## not active, check email activation
+                link = reverse('account:resend-activation')
+                reconfirm_msg = """Go here to <a href='{resend_link}'>
+                resend confirmation Email
+                </a>
+                """.format(resend_link=link)
+                confirm_email = EmailActivation.objects.filter(email=email)
+                is_confirmable = confirm_email.confirmable().exists()
+                if is_confirmable:
+                    msg1 = 'Please check your email to confirm your account or ' + reconfirm_msg.lower()
+                    raise forms.ValidationError(mark_safe(msg1))
+                email_confirm_exists = EmailActivation.objects.email_exists(email).exists()
+                if email_confirm_exists:
+                    msg2 = 'You need to confirm your email. ' + reconfirm_msg
+                    raise forms.ValidationError(mark_safe(msg2))
+                if not is_confirmable and not email_confirm_exists:
+                    raise forms.ValidationError('This user is inactive.')
+
+
         user = authenticate(request, username=email, password=password)
         if user is None:
             raise forms.ValidationError('Invalid credentials!')
