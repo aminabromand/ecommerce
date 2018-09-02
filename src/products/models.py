cb685dcfdcc57a0ad79966008c56b77bff5bef16
amin <amin.r.abromand@gmail.com>
2018-09-02 20:21:06 +0200

    completed part 199 added protected storage

--- src/products/models.py
+++ src/products/models.py
@@ -1,5 +1,7 @@
 import os
 import random
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 from django.db import models
 from django.db.models import Q
 from django.db.models.signals import pre_save, post_save
@@ -115,7 +117,9 @@ def upload_product_file_loc(instance, filename):
 
 class ProductFile(models.Model):
 	product 			= models.ForeignKey(Product)
-	file 				= models.FileField(upload_to=upload_product_file_loc)
+	file 				= models.FileField(
+								upload_to=upload_product_file_loc,
+								storage=FileSystemStorage(location=settings.PROTECTED_ROOT))
 
 	def __str__(self):
 		return self.file.name
